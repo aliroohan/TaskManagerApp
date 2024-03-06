6137ba3416760350d1132132975aafe9063fe9ad
aliroohan <aliroohan321@gmail.com>
2024-03-06 16:22:35 +0500

    Added a practice Python program

---
+++ break and continue.py
@@ -0,0 +1,13 @@
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 x", i + 1, "=", 5 * (i + 1))
+#
+# print("Loop ko chodkar nikal gaya")
+
+i = 0
+while True:
+    print(i)
+    i = i+1
+    if(i%1 == 0):
+        break
